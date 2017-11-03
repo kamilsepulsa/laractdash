c802d40a8ce3d9a6956f83d2c3a3dd20d31b5029
Rachman Kamil <kamil@sepulsa.com>
2017-11-03 14:02:57 +0700

    first commit

---
+++ resources/assets/js/components/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="app-footer">
+        <span className="ml-auto"> &copy; 2017 SEPULSA.COM</span>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
